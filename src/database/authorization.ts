3e1c77e2b0252e11f242c5d65063247b73c941f2
Oloqq <olgierdpk@gmail.com>
2022-11-13 01:44:25 +0100

    Revert "update"
    This reverts commit 856ea256d7a8f894a4bf279fc216e086c550930d.

--- src/database/authorization.ts
+++ src/database/authorization.ts
@@ -31,9 +31,9 @@ export class AuthDB {
 
   put(data: UserTokens): boolean {
     var sql = `INSERT INTO authorization(id_auth, access_token, expiry, refresh_token)
-      VALUES ('?', '?', '?', '?')
+      VALUES (?, ?, ?, ?)
       ON CONFLICT(id_auth) DO UPDATE
-        SET access_token='?2', expiry='?3', refresh_token='?4'
+        SET access_token=?2, expiry=?3, refresh_token=?4
         WHERE id_auth='?1'`;
     let success = true;
     this.db.run(sql, [data.userId, data.accessToken, data.expiry, data.refreshToken], (err: any) => {
@@ -48,7 +48,7 @@ export class AuthDB {
   }
 
   get(id: string): Promise<UserTokens> {
-    var sql = `SELECT * FROM authorization WHERE id_auth='${id}'`;
+    var sql = `SELECT * FROM authorization WHERE id_auth=${id}`;
     return new Promise((resolve, reject) =>{
       this.db.get(sql, (err: any, row: any)=>{
         if (err) {
