baceab171ccda9db85cedeb5b34b574a1bae03aa
Oloqq <olgierdpk@gmail.com>
2022-12-29 13:40:30 +0100

    create different loggers based on environment

--- src/logs.ts
+++ src/logs.ts
@@ -1,7 +1,17 @@
 import { Logger } from "../types/log";
 import simpleNodeLogger from 'simple-node-logger';
 
-export const logger: Logger = simpleNodeLogger.createSimpleLogger('logs/basic.log');
-export const dblog : Logger = simpleNodeLogger.createSimpleLogger('logs/db.log');
+function loggerByEnv(logname: string): Logger {
+  if (process.env.NODE_ENV === "test") {
+    return simpleNodeLogger.createSimpleFileLogger(`logs/test/${logname}.log`);
+  }
+  else {
+    return simpleNodeLogger.createSimpleLogger(`logs/${logname}.log`);
+  }
+}
+
+
+export const logger: Logger = loggerByEnv("logs/basic.log");
+export const dblog: Logger  = loggerByEnv("logs/db.log");
 
 export default logger;
