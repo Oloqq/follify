3a438aac4a43de0468667f463aeffe01d120b412
Oloqq <olgierdpk@gmail.com>
2022-12-29 06:56:25 +0100

    cache interface

--- src/database/caching.ts
+++ src/database/caching.ts
@@ -11,58 +11,14 @@ export class CacheDB {
     const dbPath = `.data/${dbFile}.db`;
 
     this.db = new sqlite3.Database(dbPath);
-    if (!fs.existsSync(dbPath)) {
-      log.info("Creating table authorization", dbPath);
-
-      this.db.get('PRAGMA foreign_keys = ON'); // Enable foreign keys
-
-      this.db.serialize(() => {
-        this.db.run(`
-          CREATE TABLE authorization (
-            id_auth varchar(20) NOT NULL PRIMARY KEY,
-            access_token TEXT,
-            expiry TEXT,
-            refresh_token TEXT
-          )
-        `);
-      });
-    }
   }
 
-  put(data: UserTokens): boolean {
-    var sql = `INSERT INTO authorization(id_auth, access_token, expiry, refresh_token)
-      VALUES (?, ?, ?, ?)
-      ON CONFLICT(id_auth) DO UPDATE
-        SET access_token=?2, expiry=?3, refresh_token=?4
-        WHERE id_auth='?1'`;
-    let success = true;
-    this.db.run(sql, [data.userId, data.accessToken, data.expiry, data.refreshToken], (err: any) => {
-      if (err) {
-        success = false;
-        log.error('Failed to put a user. ', err);
-      } else {
-        log.info('User data upserted for: ', data.userId);
-      }
-    });
-    return success;
+  put(artist: Artist, album: Album, tracks: Track[]): boolean {
+    return false;
   }
 
-  get(id: string): Promise<UserTokens> {
-    var sql = `SELECT * FROM authorization WHERE id_auth=${id}`;
-    return new Promise((resolve, reject) =>{
-      this.db.get(sql, (err: any, row: any)=>{
-        if (err) {
-          log.error('Failed to get user data.', err);
-          reject(false);
-        }
-        resolve({
-          userId: row.id_auth,
-          accessToken: row.access_token,
-          expiry: new Date(parseFloat(row.expiry)),
-          refreshToken: row.refresh_token,
-        });
-      });
-    });
+  get(artist: Artist, minDate: Date, maxDate: Date): Promise<Track[]>|undefined {
+    return undefined;
   }
 }
 
