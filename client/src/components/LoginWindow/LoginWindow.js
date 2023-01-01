eb26af3cff0228989bf45fef316d96384673d751
Oloqq <olgierdpk@gmail.com>
2023-01-01 21:17:30 +0100

    link in console

--- client/src/components/LoginWindow/LoginWindow.js
+++ client/src/components/LoginWindow/LoginWindow.js
@@ -4,13 +4,14 @@ import { Container } from '../../globalStyles';
 import { Img, MainWrapper } from '../LandingPage/LandingPageElements';
 import { Par, Href } from '../LoginWindow/LoginWindowElements';
 
-const AUTH_ENDPOINT = "https://accounts.spotify.com/"
-
-
 const LoginWindow = () => {
-    
     function login() {
-        fetch("http://localhost:5000/login", { method: "GET", credentials: "include", mode: "cors" }).then(console.log);
+        fetch("http://localhost:5000/login", { method: "GET", credentials: "include", mode: "cors" })
+        .then(res => res.json())
+        .then(data => {
+          console.log("body", data.redirectUrl);
+          this.history.pushState(null, data.redirectUrl);
+        });
     }
 
     return (
@@ -19,7 +20,7 @@ const LoginWindow = () => {
                 <Img src={logo} alt="logo-follify"/>
                     <Par>After clicking the button below, Spotify login page will appear for you to log in!</Par>
                 <MainWrapper>
-                        <Href href={AUTH_ENDPOINT} primary fontBig onClick={login}>Connect your spotify account!</Href>
+                        <Href primary fontBig onClick={login}>Connect your spotify account!</Href>
                 </MainWrapper>
             </Container>
         </>
